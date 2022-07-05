74dc6e0994bcf796c94c4bed9dd6b52c7d4b1b56
Flávio Ribeiro <49658066+flaviotux@users.noreply.github.com>
2022-07-05 12:36:05 -0300

    fix(oh-my-zsh): installation

--- run_once_install-oh-my-zsh.sh
+++ run_once_install-oh-my-zsh.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-if [ ![-d "/usr/share/zsh/plugins/zsh-autosuggestions" && -d "/usr/share/zsh/plugins/zsh-syntax-highlighting/"] ]; then
-  sudo pacman -S --needed zsh-autosuggestions zsh-syntax-highlighting zsh
+if [ ![-d /usr/share/zsh/plugins/zsh-autosuggestions] && ![-d /usr/share/zsh/plugins/zsh-syntax-highlighting] ]; then
+  sudo pacman -Sy --needed zsh-autosuggestions zsh-syntax-highlighting zsh
+  cp -r /usr/share/zsh/plugins/zsh-* ~/.oh-my-zsh/custom/plugins/
 fi
-
-cp -r /usr/share/zsh/plugins/zsh-* ~/.oh-my-zsh/custom/plugins/
